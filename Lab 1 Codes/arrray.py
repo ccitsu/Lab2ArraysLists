567757519539671cb4a8e91b4284abbc676f3621
nayyarahmed <ccitlms@gmail.com>
2022-12-20 16:01:19 +0300

    first commit

---
+++ Lab 1 Codes/arrray.py
@@ -0,0 +1,12 @@
+#import array as arr 
+from array import* 
+a = array('d',[1,2,3,4,5,6,7,8])
+b = array('d',[11,12,13,14,15,16,17,18,20])
+
+print(len(b))
+print(len(a))
+
+c = array('d')
+c=a+b
+print(c)
+
---
+++ P8.py
@@ -0,0 +1,5 @@
+from array import *
+print("Bytes to String: ")
+x = array('b', [65,66,67,68,69])
+s = x.tobytes()
+print(s)
